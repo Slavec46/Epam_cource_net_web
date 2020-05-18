0bf1157567a5034e957f3c60c90237034747d95a
Slavec46 <41571219+Slavec46@users.noreply.github.com>
2020-05-19 00:01:01 +0400

    Create task 1_1_2
    Homework to creating task about right triangle

---
+++ Practice1_1_1/Practice1_1/Task1_1_2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1_1_1
+{
+    class Task1_1_2
+    {
+        static void Main()
+        {
+            int i = 0;
+            int f = 0;
+            Console.Write("N = ");
+            int.TryParse(Console.ReadLine(), out int n);
+            while (i <= n)
+            {
+                while (f < n)
+                {
+                    if (i > f)
+                    {
+                        Console.Write("*");
+                    }
+                    f++;
+                }
+                Console.WriteLine();
+                f = 0;
+                i++;
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
