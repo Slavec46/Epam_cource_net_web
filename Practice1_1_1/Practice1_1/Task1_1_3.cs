71166bc4fdb750f48d583385da27008d455b606d
Slavec46 <41571219+Slavec46@users.noreply.github.com>
2020-05-25 16:15:23 +0400

    Create Task1_1_3

---
+++ Practice1_1_1/Practice1_1/Task1_1_3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Practice1_1
+{
+    class Task1_1_3
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N: ");
+
+
+            if (ulong.TryParse(Console.ReadLine(), out var Number))
+            {
+                Console.WriteLine("");
+                ShowTriangle(Number);
+            }
+            else
+            {
+                Console.WriteLine("Enter a positive number");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void ShowTriangle(ulong number)
+        {
+            if (number != 0)
+            {
+                for (ulong j = 1; number > 0; j+=2, number--)
+                {
+                    Console.WriteLine(new String(' ',(int)number) + new String('*', (int)j));
+                }
+            }
+            else
+            {
+                Console.WriteLine("is nothing to draw");
+            }
+        }
+    }
+}
