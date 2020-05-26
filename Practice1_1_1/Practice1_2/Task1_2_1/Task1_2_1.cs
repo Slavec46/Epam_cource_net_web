457ef8d3e8370fd0aa150cd5bf80af150afaeee9
Slavec46 <41571219+Slavec46@users.noreply.github.com>
2020-05-26 14:21:44 +0400

    Create Task1_2_1

---
+++ Practice1_1_1/Practice1_2/Task1_2_1/Task1_2_1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice1_2
+{
+    class Task1_2_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Averages");
+            Console.WriteLine(new string('-', 20));
+
+            Console.WriteLine("Enter text:");
+
+            string str = Console.ReadLine();
+
+            Console.WriteLine(new string('-', 20));
+
+            char[] simbols = { ' ', '.', ',', '!', '?', '"', '\'', ':', ';' };
+            List<StringBuilder> list = new List<StringBuilder>();
+
+            // Деление на слова
+            int Count = 0;
+            foreach (var item in str.Split(simbols, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (string.IsNullOrWhiteSpace(item)) continue;
+
+                list.Add(new StringBuilder());
+
+                // Устранение знаков припинаний
+                foreach (char simbol in item)
+                {
+                    if (char.IsSeparator(simbol)) break;
+
+                    list[Count].Append(simbol);
+                }
+
+                Count++;
+            }
+
+            Count = 0;
+            foreach (var item in list)
+            {
+                Count += item.Length;
+            }
+            // был выбран подсчет с округлением
+            Count /= list.Count;
+
+
+            Console.WriteLine($"Average long of word: {Count}");
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
