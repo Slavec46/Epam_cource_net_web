20d7977b6b8db4831ae911516f2e18e0348481f4
Slavec46 <41571219+Slavec46@users.noreply.github.com>
2020-05-26 13:24:37 +0400

    Create Task1_1_9

---
+++ Practice1_1_1/Practice1_1/Task1_1_9.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Practice1_1
+{
+    class Task1_1_9
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { -9, 4, -11, 0, 0, 1, -2, 2, -6, 1 };
+
+            Console.WriteLine("Negative sum");
+            Console.WriteLine(new string('-', 20));
+
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(new string('-', 20));
+
+            Console.WriteLine($"Sum: { SumPositiveElements(array) }");
+
+            Console.ReadKey();
+        }
+
+        /// Возвращает сумму положительных элементов массива       
+        static int SumPositiveElements(int[] array)
+        {
+            int sum = 0;
+
+            foreach (int item in array)
+            {
+                if (item > 0)
+                    sum += item;
+            }
+
+            return sum;
+        }
+    }
+}
