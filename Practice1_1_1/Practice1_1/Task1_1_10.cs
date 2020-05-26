cea666711804e49f672e2a28512b964028132eac
Slavec46 <41571219+Slavec46@users.noreply.github.com>
2020-05-26 13:41:00 +0400

    Create Task1_1_10

---
+++ Practice1_1_1/Practice1_1/Task1_1_10.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Practice1_1
+{
+    class Task1_1_10
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sum of even element numbers");
+            Console.WriteLine(new string('-', 20));
+
+            int[,] array = new int[5, 5];
+            int sum = 0;
+
+            Console.WriteLine($"x: {array.GetLength(0)}, y: {array.GetLength(1)}");
+
+            Console.WriteLine(new string('-', 20));
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (((i + j) % 2) == 0)
+                        sum += i + j;
+                }
+            }
+
+            Console.WriteLine($"Sum: { sum }");
+
+            Console.ReadKey();
+        }
+    }
+}
