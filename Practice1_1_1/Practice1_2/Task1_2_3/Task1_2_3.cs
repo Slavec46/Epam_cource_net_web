fa54e217bc9bd92563518b520bd68a91d2bc0d8b
Slavec46 <41571219+Slavec46@users.noreply.github.com>
2020-05-26 14:50:06 +0400

    Create Task1_2_3

---
+++ Practice1_1_1/Practice1_2/Task1_2_3/Task1_2_3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Practice1_2
+{
+    class Task1_2_3
+    {
+        static void Main(string[] args)
+        {
+            string str = "Антон хорошо начал утро: послушал Стинга, выпил кофе и посмотрел Звёздные Войны";
+
+            char[] simbols = { ' ', '.', ',', '!', '?', '"', '\'', ':', ';' };
+
+            string[] strs = str.Split(simbols, StringSplitOptions.RemoveEmptyEntries);
+
+
+            int Count = 0;
+            for (int i = 0; i < strs.Length; i++)
+            {
+                foreach (var item in strs[i])
+                {
+                    if (char.IsLower(item))
+                    {
+                        Count++;
+                    }
+
+                    break;
+                }
+            }
+
+            Console.WriteLine("Lowercase");
+            Console.WriteLine(new string('-', 20));
+
+            Console.WriteLine("Text: ");
+            Console.WriteLine(str);
+
+            Console.WriteLine(new string('-', 20));
+
+            Console.WriteLine("Sum: " + Count);
+
+            Console.ReadKey();
+        }
+    }
+}
