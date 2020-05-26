feb7bd5bfbb2732874608c20ac210c39681d361e
Slavec46 <41571219+Slavec46@users.noreply.github.com>
2020-05-26 14:36:24 +0400

    Create Task 1_2_2

---
+++ Practice1_1_1/Practice1_2/Task1_2_2/Task1_2_2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Practice1_2
+{
+    class Task1_2_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Doubler");
+            Console.WriteLine(new string('-', 25));
+
+            char[] simbols1;
+            Console.Write("Enter text 1: ");
+            simbols1 = Console.ReadLine().ToCharArray();
+
+            char[] simbols2;
+            Console.Write("Enter text 2: ");
+            simbols2 = Console.ReadLine().Distinct().ToArray();
+
+            Console.WriteLine(new string('-', 25));
+
+            for (int i1 = 0; i1 < simbols1.Length; i1++)
+            {
+                for (int i2 = 0; i2 < simbols2.Length; i2++)
+                {
+                    if (simbols1[i1] == simbols2[i2])
+                    {
+                        Console.Write(simbols1[i1]);
+                    }
+                }
+
+                Console.Write(simbols1[i1]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
