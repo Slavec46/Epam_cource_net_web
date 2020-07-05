3d2e240769b8b8cf55104189b811140897177e26
Slavec46 <41571219+Slavec46@users.noreply.github.com>
2020-07-05 19:31:45 +0400

    Finish Task 3_2_1
    Rebuild class MyEnumerator and create other functional

--- Practice 3/3_2_1_Dynamic_Array/3_2_1_Dynamic_Array/Program.cs
+++ Practice 3/3_2_1_Dynamic_Array/3_2_1_Dynamic_Array/Program.cs
@@ -5,8 +5,17 @@ namespace _3_2_1_Dynamic_Array
     class Program
     {
         static void Main(string[] args)
-        {   
-            Console.WriteLine("Hello World!");
+        {
+            MyDynamicArray<int> array = new MyDynamicArray<int>(new int[] { 2, 3, 4 });
+
+
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
         }
     }
 }
