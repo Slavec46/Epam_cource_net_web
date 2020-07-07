7a768876c199bfe7c4f43dbc300a78e7ae5157e1
Slavec46 <41571219+Slavec46@users.noreply.github.com>
2020-07-07 14:13:44 +0400

    Start Creating task 3_3_1

---
+++ Practice 3/3_3_1_Super_Array/3_3_1_Super_Array/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3_3_1_Super_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
