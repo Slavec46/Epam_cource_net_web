1c6250b9a175e131de0f7c8b25343f73816f4d3b
Slavec46 <41571219+Slavec46@users.noreply.github.com>
2020-07-12 13:44:31 +0400

    Finish task 3_3_1
    Created classes "ArrayExtension" & "Program"

---
+++ Practice 3/Task_3_3_1_Super_Array/3_3_1_Super_Array/ArrayExtension.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3_3_1_Super_Array
+{
+    public static class ArrayExtension
+    {
+        public delegate double ChangeElements(double element); // delegate perform action on elements
+        public delegate double SearchValues(double[] array); // delegate for search values
+
+        public static double[] ChangeArrayElements(this double[] array, ChangeElements changeEl)
+        {
+            double[] newArray = new double[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = changeEl.Invoke(array[i]);
+            }
+            return newArray;
+        }
+        public static double MultiplyByTwo(double n) // for extension method "ChangeArrayElements"
+        {
+            return n * 2;
+        }
+        public static double SquareIt(double n) // for extension method "ChangeArrayElements"
+        {
+            return n * n;
+        }
+
+        public static double SearchArrayValues(this double[] array, SearchValues searchVal)
+        {
+            return searchVal.Invoke(array);
+        }
+        public static double SumArrayElements(double[] array) // for extension method "SearchArrayValues" - search sum of values
+        {
+            double sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+        public static double AverageArrayElements(double[] array)  // for extension method "SearchArrayValues" - search everage value
+        {
+            double average = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                average += array[i];
+            }
+            return average / array.Length;
+        }
+        public static double CommonArrayElement(double[] array) // for extension method "SearchArrayValues" - search most common values
+        {
+            int count = 0;
+            int index = -1;
+            for (int i = 0; i < array.Length; ++i)
+            {
+                int k = 1;
+                for (int j = i + 1; j < array.Length; ++j)
+                {
+                    if (array[i] == array[j]) k++;
+                }
+                if (k <= count) continue;
+                count = k;
+                index = i;
+            }
+            return array[index];
+        }
+    }
+}
--- Practice 3/Task_3_3_1_Super_Array/3_3_1_Super_Array/Program.cs
+++ Practice 3/Task_3_3_1_Super_Array/3_3_1_Super_Array/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using static System.Console;
 
 namespace _3_3_1_Super_Array
 {
@@ -6,7 +6,38 @@ namespace _3_3_1_Super_Array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UseExtensionMethods();
+        }
+        private static void UseExtensionMethods()
+        {
+            double[] array = new double[] { 1, 2, 2, 3, 3, 3, 4, 5 };
+            WriteLine("Исходный массив:");
+            foreach (var item in array)
+            {
+                Write(item + "   ");
+            }
+            WriteLine();
+            double[] array2 = array.ChangeArrayElements(ArrayExtension.MultiplyByTwo);
+            WriteLine("Массив после обработки MultiplyByTwo:");
+            foreach (var item in array2)
+            {
+                Write(item + "   ");
+            }
+            WriteLine();
+            double[] array3 = array.ChangeArrayElements(ArrayExtension.SquareIt);
+            WriteLine("Массив после обработки SquareIt:");
+            foreach (var item in array3)
+            {
+                Write(item + "   ");
+            }
+            WriteLine();
+            double sum = array.SearchArrayValues(ArrayExtension.SumArrayElements);
+            WriteLine("Сумма элементов исходного массива равна " + sum);
+            double average = array.SearchArrayValues(ArrayExtension.AverageArrayElements);
+            WriteLine("Среднее значение элементов исходного массива равно " + average);
+            double common = array.SearchArrayValues(ArrayExtension.CommonArrayElement);
+            WriteLine("Наиболее часто повторяющееся значение элементов исходного массива равно " + common);
+            ReadKey();
         }
     }
 }
