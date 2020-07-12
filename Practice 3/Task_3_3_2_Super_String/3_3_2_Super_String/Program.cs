a036526e3c4790de1c57e012d11c96a47bba1dee
Slavec46 <41571219+Slavec46@users.noreply.github.com>
2020-07-12 19:56:59 +0400

    Finish Task 3_3_2
    Created classes "StringExtension" & "Program"

---
+++ Practice 3/Task_3_3_2_Super_String/3_3_2_Super_String/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+
+namespace _3_3_2_Super_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputToConsole();
+        }
+        static void OutputToConsole()
+        {
+            string phrase1 = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+            string phrase2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string phrase3 = "0123456789";
+            string phrase4 = "v w";
+            Languages check = phrase1.CheckLanguage();
+            WriteLine("Phrase: " + phrase1);
+            WriteLine("Language: " + check);
+            WriteLine();
+            check = phrase2.CheckLanguage();
+            WriteLine("Phrase: " + phrase2);
+            WriteLine("Language: " + check);
+            WriteLine();
+            check = phrase3.CheckLanguage();
+            WriteLine("Phrase: " + phrase3);
+            WriteLine("Language: " + check);
+            WriteLine();
+            check = phrase4.CheckLanguage();
+            WriteLine("Phrase: " + phrase4);
+            WriteLine("Language: " + check);
+            ReadKey();
+        }
+    }
+}
---
+++ Practice 3/Task_3_3_2_Super_String/3_3_2_Super_String/StringExtension.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace _3_3_2_Super_String
+{
+    public static class StringExtension
+    {
+        public static Languages CheckLanguage(this string phrase)
+        {
+            phrase = phrase.ToLower();
+            byte[] symbol = Encoding.Default.GetBytes(phrase);
+
+            if (symbol.All(letter => letter >= 48 && letter <= 57))
+            {
+                return Languages.Number;
+            }
+            else if (symbol.All(letter => letter >= 97 && letter <= 122))
+            {
+                return Languages.English;
+            }
+            else if (symbol.All(letter => (letter >= 192 && letter <= 255) || letter == 184))
+            {
+                return Languages.Russian;
+            }
+            else
+            {
+                return Languages.Mixed;
+            }
+        }
+    }
+}
