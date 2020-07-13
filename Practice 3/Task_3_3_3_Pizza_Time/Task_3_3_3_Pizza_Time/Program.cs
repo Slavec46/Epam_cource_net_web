ac4572fb61ec8c46f9a9373184ddcd1e923dd62a
Slavec46 <41571219+Slavec46@users.noreply.github.com>
2020-07-13 13:24:23 +0400

    Create Task 3_3_3

---
+++ Practice 3/Task_3_3_3_Pizza_Time/Task_3_3_3_Pizza_Time/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_3_3_3_Pizza_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
