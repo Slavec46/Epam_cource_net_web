b68d0073a152fa3b1c9cdeeed2c496678476b0ed
Slavec46 <41571219+Slavec46@users.noreply.github.com>
2020-07-22 12:50:46 +0400

    Create Task 4_1_1_FIle_Management_System

---
+++ Practice 4/Task_4_1_1_File_Management_System/Task_4_1_1_File_Management_System/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_4_1_1_File_Management_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
