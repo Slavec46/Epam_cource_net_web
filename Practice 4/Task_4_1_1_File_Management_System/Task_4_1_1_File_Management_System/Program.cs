64ec05204e2508224b9a303855e3792370f00815
Slavec46 <41571219+Slavec46@users.noreply.github.com>
2020-08-01 14:22:24 +0400

    Finished Task 4_1_1
    Create class "Program"

--- Practice 4/Task_4_1_1_File_Management_System/Task_4_1_1_File_Management_System/Program.cs
+++ Practice 4/Task_4_1_1_File_Management_System/Task_4_1_1_File_Management_System/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.IO;
+
 
 namespace Task_4_1_1_File_Management_System
 {
     class Program
     {
-        static void Main(string[] args)
+        private static bool flagForWrite = true;
+        private static string pathToCatalog;
+
+        static void Main() // Прежде чем запускать проверьте в режиме отладки верное ли значение Type в классе Restoration.cs
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                if (flagForWrite)
+                {
+                    flagForWrite = false;
+                    Console.WriteLine("Введите путь к каталогу: ");
+                    pathToCatalog = Console.ReadLine();
+                }
+
+                var tracker = new Tracker(@pathToCatalog);
+                Console.ReadLine();
+                tracker.Dispose();
+            }
+            #region catch
+            catch
+                (DirectoryNotFoundException
+                exception)
+            {
+                Console.WriteLine(exception.Message);
+
+                Console.WriteLine("Создать его? y/n");
+
+                if (Console.ReadKey().Key == ConsoleKey.Y)
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(exception.Message);
+                        Console.Clear();
+
+                        Main();
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+            #endregion
         }
     }
 }
